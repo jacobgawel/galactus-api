8f0facce3969c3bae91abe4f71fe90e33489fdfe
Jakub Gawel <jakubgawel@icloud.com>
2025-12-15 01:15:14 +0000

    chore: added README.md, removed unused method from GroupRepository.java

---
+++ README.md
@@ -0,0 +1,8 @@
+## Galactus API
+#### Current tech stack used
+- SQL Server 2025
+- Redis Cache
+- GraphQL
+- Spring Boot 4.0.0
+- Java version 25
+
--- src/main/java/com/galactus/persistence/GroupRepository.java
+++ src/main/java/com/galactus/persistence/GroupRepository.java
@@ -3,9 +3,5 @@ package com.galactus.persistence;
 import com.galactus.domain.database.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface GroupRepository extends JpaRepository<Group, Long> {
-    Optional<Group> findGroupById(Long id);
 }
